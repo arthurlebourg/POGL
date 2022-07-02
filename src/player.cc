ea3bf3a75897f4b49d0ec32b62d85d2b910a3392
jiaia-ao <jiayihao.jiaiaao@gmail.com>
2022-07-02 11:14:20 +0200

    [CLEAN] ready for merge

--- src/player.cc
+++ src/player.cc
@@ -91,16 +91,19 @@ btRigidBody *Player::get_body()
     return body_;
 }
 
-float calculate_angleRotY(glm::vec3 direction)
+float calculate_angleRotX(glm::vec3 direction)
 {
     auto direction_along_z = glm::vec3(direction.x, 0, direction.z);
+    auto norm_dir = sqrt(direction.x * direction.x + direction.y * direction.y + direction.z * direction.z);
+    auto norm_z = sqrt(direction_along_z.x * direction_along_z.x + direction_along_z.y * direction_along_z.y + direction_along_z.z * direction_along_z.z);
     return acos(glm::dot(direction, direction_along_z)
-           / glm::length(direction) * glm::length(direction_along_z));
+           / (norm_dir * norm_z));
 }
 
 glm::vec3 apply_rotation(const glm::vec3 direction, const float angle)
 {
     auto rotation = glm::mat3(1, 0, 0, 0, cos(angle), -sin(angle), 0, sin(angle), cos(angle));
+    // auto rotation = glm::mat3(cos(angle), -sin(angle), 0, sin(angle), cos(angle), 0, 0, 0, 1);
     return rotation * direction;
 }
 
@@ -110,13 +113,18 @@ void Player::move(const int forward, const int sideward, const float deltaTime)
     btVector3 vel = body_->getLinearVelocity();
 
     // angleRotX = angle(playerLookingDirection, playerLookingStraightForwardDirection) = angle(direction_, direction_along_z)
-    auto angleRotX = calculate_angleRotY(direction_);
+    auto angleRotX = calculate_angleRotX(direction_);
 
     // rotate looking direction to moving direction
     auto correctedDir = apply_rotation(direction_, angleRotX);
 
     glm::vec3 dir = forward * speed_ * deltaTime * correctedDir
         + sideward * speed_ * deltaTime * glm::normalize(glm::cross(correctedDir, up_));
+
+
+    // glm::vec3 dir = forward * speed_ * deltaTime * direction_
+    //     + sideward * speed_ * deltaTime
+    //         * glm::normalize(glm::cross(direction_, up_));
 
     vel.setX(dir.x);
     vel.setZ(dir.z);
