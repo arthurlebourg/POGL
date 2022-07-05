a14c9e42c94efb4ca586d977b7bf2909557e0802
Arthur Le Bourg <lebourgarthur@gmail.com>
2022-07-06 01:05:22 +0200

    added last scene

--- include/scene_builder.hh
+++ include/scene_builder.hh
@@ -2,6 +2,9 @@
 
 #include "program.hh"
 
+std::shared_ptr<Program> portals(std::string vertex_src,
+                                 std::string fragment_src);
+
 std::shared_ptr<Program> long_tunnel(std::string vertex_src,
                                      std::string fragment_src);
 
--- src/main.cc
+++ src/main.cc
@@ -6,8 +6,9 @@ int main(int argc, char *argv[])
     init_glew();
     initGL();
 
-    std::shared_ptr<Program> prog = long_tunnel("vertex.shd","fragment.shd");
+    //std::shared_ptr<Program> prog = long_tunnel("vertex.shd","fragment.shd");
     //std::shared_ptr<Program> prog = amongus("vertex.shd", "fragment.shd");
+    std::shared_ptr<Program> prog = portals("vertex.shd", "fragment.shd");
 
     while (!prog->is_ready())
     {}
